fb1cb28f784993d88609f22a2410f4968b847e32
Peter Khotpanya <pkhotpan@gmail.com>
2016-10-26 13:25:48 -0700

    ViewController and Multiple Views: Add navigation bar and image assets
    -Added record button and stop record button image into image Asses.
    -Added navigation bar and another view controller to be pushed on stop
    recording buttons press.
    -Enable stopRecordingButton and disable recordButton when recording
    audio.
    -Disable stopRecordingButton and enable recordButton when not recording
    audio.
    -Disable stopRecordingButton on viewDidLoad.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +31,14 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: AnyObject) {
         recordingLabel.text = "Recording in progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
 
     @IBAction func stopRecording(_ sender: AnyObject) {
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
 
 }
